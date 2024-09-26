e14c9444f53afa74de4732442e150088f8a42746
Alena <krostarmotor@gmail.com>
2024-09-26 10:56:54 +0200

    1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-require('dotenv').config();
+
 
 root.render(
   <React.StrictMode>
